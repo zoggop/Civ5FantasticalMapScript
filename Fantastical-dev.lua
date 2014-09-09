011a442c78656982fa2b3d933b7a7190e5a8287c
zoggop <zoggop@gmail.com>
2014-09-08 17:50:34 -0700

    markov names from civ city name lists

--- Fantastical-dev.lua
+++ Fantastical-dev.lua
@@ -112,7 +112,144 @@ local function AngleDist(angle1, angle2)
 	return mAbs((angle1 + mPi -  angle2) % mTwicePi - mPi)
 end
 
+------------------------------------------------------------------------------
+-- FOR CREATING CITY NAMES: MARKOV CHAINS
+-- ADAPTED FROM drow <drow@bin.sh> http://donjon.bin.sh/code/name/
+-- http://creativecommons.org/publicdomain/zero/1.0/
 
+local name_set = {}
+local name_types = {}
+local chain_cache = {}
+
+local function splitIntoWords(s)
+  local words = {}
+  for w in s:gmatch("%S+") do tInsert(words, w) end
+  return words
+end
+
+
+local function scale_chain(chain)
+  local table_len = {}
+  for key in pairs(chain) do
+    table_len[key] = 0
+    for token in pairs(chain[key]) do
+      local count = chain[key][token]
+      local weighted = math.floor(math.pow(count,1.3))
+      chain[key][token] = weighted
+      table_len[key] = table_len[key] + weighted
+    end
+  end
+  chain['table_len'] = table_len
+  return chain
+end
+
+local function incr_chain(chain, key, token)
+  if chain[key] then
+    if chain[key][token] then
+      chain[key][token] = chain[key][token] + 1
+    else
+      chain[key][token] = 1
+    end
+  else
+    chain[key] = {}
+    chain[key][token] = 1
+  end
+  return chain
+end
+
+-- construct markov chain from list of names
+local function construct_chain(list)
+  local chain = {}
+  for i = 1, #list do
+    local names = splitIntoWords(list[i])
+    chain = incr_chain(chain,'parts',#names)
+    for j = 1, #names do
+      local name = names[j]
+      chain = incr_chain(chain,'name_len',name:len())
+      local c = name:sub(1, 1)
+      chain = incr_chain(chain,'initial',c)
+      local string = name:sub(2)
+      local last_c = c
+      while string:len() > 0 do
+        local c = string:sub(1, 1)
+        chain = incr_chain(chain,last_c,c)
+        string = string:sub(2)
+        last_c = c
+      end
+    end
+  end
+  return scale_chain(chain)
+end
+
+function select_link(chain, key)
+  local len = chain['table_len'][key]
+  if not len then return '-' end
+  local idx = math.floor(mRandom() * len)
+  local t = 0
+  for token in pairs(chain[key]) do
+    t = t + chain[key][token]
+    if idx < t then return token end
+  end
+  return '-'
+end
+
+-- construct name from markov chain
+local function markov_name(chain)
+  local parts = select_link(chain,'parts')
+  local names = {}
+  for i = 1, parts do
+    local name_len = select_link(chain,'name_len')
+    local c = select_link(chain,'initial')
+    local name = c
+    local last_c = c
+    while name:len() < name_len do
+      c = select_link(chain,last_c)
+      name = name .. c
+      last_c = c
+    end
+    table.insert(names, name)
+  end
+  local nameString = ""
+  for i, name in ipairs(names) do nameString = nameString .. name .. " " end
+  nameString = nameString:sub(1,-2)
+  return nameString
+end
+
+-- get markov chain by type
+local function markov_chain(type)
+  local chain = chain_cache[type]
+  if chain then
+    return chain
+  else
+    local list = name_set[type]
+    if list then
+      local chain = construct_chain(list)
+      if chain then
+        chain_cache[type] = chain
+        return chain
+      end
+    end
+  end
+  return false
+end
+
+-- generator function
+local function generate_name(type)
+  local chain = markov_chain(type)
+  if chain then
+    return markov_name(chain)
+  end
+  return ""
+end
+
+-- generate multiple
+local function name_list(type, n_of)
+  local list = {}
+  for i = 1, n_of do
+    table.insert(list, generate_name(type))
+  end
+  return list
+end
 
 ------------------------------------------------------------------------------
 
@@ -166,31 +303,35 @@ end
 local LabelSyntaxes = {
 	{ "Place", " of ", "Noun" },
 	{ "Adjective", " ", "Place"},
+	{ "Name", " ", "Place"},
+	{ "Adjective", " ", "Place", " of ", "Name"},
 }
 
 local LabelDictionary ={
 	Place = {
-		Land = { "Country", "Vale" },
+		Land = { "Lands", "Vale" },
 		Sea = { "Sea", "Shallows", "Reef" },
 		Islet = { "Islet", "Key", "Cay" },
 		Island = { "Island", "Isle" },
 		Archipelago = { "Archipelago", },
-		Mountains = { "Heights", "Highlands" },
+		Mountains = { "Heights", "Highlands", "Spires", "Crags" },
 		Hills = { "Hills", "Plateau", "Fell" },
 		Dunes = { "Dunes", "Sands", "Drift" },
 		Plains = { "Plain", "Prarie", "Steppe", "Flat", "Field" },
-		Forest = { "Forest", "Wood", "Grove", "Thicket" },
+		Forest = { "Forest", "Wood", "Grove", "Thicket", "Veldt" },
 		Jungle = { "Jungle", "Maze", "Tangle" },
-		Swamp = { "Swamp", "Marsh", "Down" },
-		Range = { "Mountains", "Range", "Sierra", "Crest" },
+		Swamp = { "Swamp", "Marsh", "Down", "Fen" },
+		Range = { "Mountains", "Range", "Crest" },
 		Ocean = { "Ocean", "Ocean", "Ocean", "Ocean" },
+		Waste = { "Waste", "Desolation" },
+		Grassland = { "Heath" },
 	},
 	Noun = {
 		Unknown = { "Despair", "Light" },
-		Hot = { "Brimstone", "The Sun", "The Anvil" },
-		Cold = { "Frost", "Snow", "Ice" },
+		Hot = { "The Sun", "The Anvil" },
+		Cold = { "Frost", "Crystal" },
 		Wet = { "Cloud", "Fog", "Monsoons" },
-		Dry = { "Dust", "Bones" },
+		Dry = { "Dust", "Withering" },
 		Big = { "The Ancients", "The Unfathomable" },
 		Small = { "Crossings" },
 	},
@@ -200,7 +341,7 @@ local LabelDictionary ={
 		Cold = { "Snowy", "Icy", "Frigid" },
 		Wet = { "Damp", "Misty", "Stormy" },
 		Dry = { "Dusty", "Dry", "Parched" },
-		Big = { "Elder", "Wide" },
+		Big = { "Wide" },
 		Small = { "Lesser" },
 	}
 }
@@ -209,6 +350,7 @@ local LabelDefinitions -- has to be set in SetConstants()
 
 local function EvaluateCondition(key, condition, thing)
 	if type(condition) == "boolean" then
+		if not condition then return not thing[key] end
 		return thing[key]
 	end
 	if thing[key] then
@@ -251,9 +393,6 @@ local function GetLabel(thing)
 						break
 					end
 				end
-			end
-			if thisKind == "Plains" then
-				EchoDebug("plains jungle ratio: ", thing.featureRatios[featureJungle])
 			end
 			if not thisKind then thisKind = "Unknown" end
 			if LabelDictionary[part][thisKind] and #LabelDictionary[part][thisKind] > 0 then
@@ -480,8 +619,6 @@ local featureForest, featureJungle, featureIce, featureMarsh, featureOasis, feat
 local TerrainDictionary, FeatureDictionary
 
 local function SetConstants()
-	EchoDebug(string.sub("hello, ", 1, -3))
-
 	FlowDirN, FlowDirNE, FlowDirSE, FlowDirS, FlowDirSW, FlowDirNW = FlowDirectionTypes.FLOWDIRECTION_NORTH, FlowDirectionTypes.FLOWDIRECTION_NORTHEAST, FlowDirectionTypes.FLOWDIRECTION_SOUTHEAST, FlowDirectionTypes.FLOWDIRECTION_SOUTH, FlowDirectionTypes.FLOWDIRECTION_SOUTHWEST, FlowDirectionTypes.FLOWDIRECTION_NORTHWEST
 	FlowDirNames = {
 		[FlowDirN] = "North",
@@ -577,6 +714,48 @@ local function SetConstants()
 		Big = { oceanSize = 6 },
 		Small = { oceanSize = -7 }
 	}
+end
+
+local function GetCityNames(numberOfCivs)
+	numberOfCivs = numberOfCivs or 1
+	local civTypeGot = {}
+	local civTypes = {}
+	for value in GameInfo.Civilization_CityNames() do
+		for k, v in pairs(value) do
+			if k == "CivilizationType" then
+				if not civTypeGot[v] then tInsert(civTypes, v) end
+				civTypeGot[v] = true
+			end
+		end
+	end
+	local cityNames = {}
+	local civs = {}
+	local n = 0
+	repeat
+		local cNames = {}
+		local civType = tRemoveRandom(civTypes)
+		EchoDebug(civType)
+		for value in GameInfo.Civilization_CityNames("CivilizationType='" .. civType .. "'") do
+			for k, v in pairs(value) do
+				-- TXT_KEY_CITY_NAME_ARRETIUM
+				local begOfCrap, endOfCrap = string.find(v, "CITY_NAME_")
+				if endOfCrap then
+					local name = string.sub(v, endOfCrap+1)
+					name = string.gsub(name, "_", " ")
+					name = string.lower(name)
+					name = name:gsub("(%l)(%w*)", function(a,b) return string.upper(a)..b end)
+					-- if k == "CityName" then EchoDebug(name) end
+					tInsert(cNames, name)
+				end
+			end
+		end
+		if #cNames > 5 then
+			cityNames[civType] = cNames
+			tInsert(civs, civType)
+			n = n + 1
+		end
+	until n == numberOfCivs
+	return cityNames, civs
 end
 
 ------------------------------------------------------------------------------
@@ -1577,6 +1756,7 @@ function Region:Label()
 	if self.polygons[1].continent then
 		self.continentSize = #self.polygons[1].continent
 	end
+	self.astronomyIndex = self.polygons[1].astronomyIndex
 	self.plotCounts = {}
 	for i = -1, PlotTypes.NUM_PLOT_TYPES - 1 do
 		self.plotCounts[i] = 0
@@ -2801,6 +2981,17 @@ function Space:FillRegions()
 end
 
 function Space:LabelMap()
+	EchoDebug("generating names...")
+	name_set, name_types = GetCityNames(self.totalAstronomyBasins + 1)
+	LabelDictionary.Name = {}
+	for i, type in ipairs(name_types) do
+		LabelDictionary.Name[type] = name_list(type, 16)
+		if i > self.totalAstronomyBasins then
+			LabelDefinitions[type] = { astronomyIndex = false }
+		else
+			LabelDefinitions[type] = { astronomyIndex = i }
+		end
+	end
 	if GameInfo.Fantastical_Map_Labels then
 		DatabaseQuery("DROP TABLE Fantastical_Map_Labels;")
 	end
@@ -2855,7 +3046,7 @@ function Space:LabelMap()
 			temperatureAvg = temperatureAvg / tempCount
 			rainfallAvg = rainfallAvg / rainCount
 			EchoDebug("valid mountain range: ", #range, temperatureAvg, temperatureAvg)
-			local thing = { rangeLength = #range, x = x, y = y, rainfallAvg = rainfallAvg, temperatureAvg = temperatureAvg }
+			local thing = { rangeLength = #range, x = x, y = y, rainfallAvg = rainfallAvg, temperatureAvg = temperatureAvg, astronomyIndex = range[1].polygons[1].astronomyIndex }
 			if LabelThing(thing) then rangesLabelled = rangesLabelled + 1 end
 		end
 	until #rangeBuffer == 0 or rangesLabelled >= self.rangeLabelsMax
@@ -3771,6 +3962,11 @@ local mySpace
 function GeneratePlotTypes()
     print("Generating Plot Types (Fantastical) ...")
 	SetConstants()
+	-- name_set, name_types = GetCityNames(2)
+	-- for i = 1, 12 do
+		-- local type = tGetRandom(name_types)
+		-- EchoDebug(generate_name(type), type)
+	-- end
     mySpace = Space()
     mySpace:SetOptions(OptionDictionary)
     mySpace:Compute()
