ba133a059c2d529dad70e96b1ac73477d95852d6
eronoobos <eronoobos@gmail.com>
2018-01-16 18:34:08 -0800

    prevent infinite mountain resize loop when more cores than prescribed

--- Fantastical-dev.lua
+++ Fantastical-dev.lua
@@ -1,6 +1,6 @@
 -- Map Script: Fantastical
--- Author: zoggop
--- version 24
+-- Author: eronoobos
+-- version 25
 
 --------------------------------------------------------------
 if include == nil then
@@ -4766,11 +4766,12 @@ function Space:DisperseFakeLatitude()
 end
 
 function Space:ResizeMountains(prescribedArea)
+	local stubbornMountainHexLimit = mMin(#self.mountainCoreHexes, prescribedArea)
 	if #self.mountainHexes == prescribedArea then return end
 	if #self.mountainHexes > prescribedArea then
 		repeat
 			local hex = tRemoveRandom(self.mountainHexes)
-			if hex.mountainRangeCore and #self.mountainCoreHexes > 0 and #self.mountainHexes > #self.mountainCoreHexes then
+			if hex.mountainRangeCore and #self.mountainCoreHexes > 0 and #self.mountainHexes > stubbornMountainHexLimit then
 				tInsert(self.mountainHexes, hex)
 			else
 				if Map.Rand(10, "hill dice") < self.hillChance then
